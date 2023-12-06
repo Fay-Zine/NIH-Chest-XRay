1ebc533788feefef13ec087a055996a4e8efa159
Gilbert Jabbour <gilbert.jabbour@gmail.com>
2023-12-06 00:21:29 -0500

    CNN final

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from torch.utils.data import DataLoader
 import pandas as pd
 import wandb
 import random
+from sklearn.metrics import classification_report
 
 
 # Function to train and evaluate the model
@@ -22,24 +23,23 @@ def train():
         torch.manual_seed(config["random_seed"])
         np.random.seed(config["random_seed"])
         random.seed(config["random_seed"])
-
         num_epochs = config["num_epochs"]
 
         # Load data
-        train_dataset, val_dataset, test_dataset = load_data(
-            3
-        )  # Specify the number of classes, either 3 or 5
-        train_loader, val_loader, test_loader = get_dataloaders(
-            train_dataset, val_dataset, test_dataset, config["batch_size"]
-        )
+        print("Loading data...")
+        train_dataset, val_dataset = load_data()
+        train_loader, val_loader = get_dataloaders(train_dataset, val_dataset, config["batch_size"])
 
         # initialize the model
         model, criterion, optimizer = initialize_CNN(config)
 
         # Early stopping
         early_stopping = EarlyStopping(
-            patience=5, verbose=True, config=config, run_name=run_name
+            patience=3, verbose=True, config=config, run_name=run_name
         )
+
+        # Initialize variables to track the best metrics
+        best_metrics = None
 
         # Training loop
         print("Training model...")
@@ -53,6 +53,7 @@ def train():
             for i, (inputs, labels) in enumerate(train_loader, 1):
                 # Forward pass
                 outputs = model(inputs)
+                labels = labels.float()
                 loss = criterion(outputs, labels)
 
                 # Backward and optimize
@@ -62,8 +63,8 @@ def train():
 
                 running_loss += loss.item()
 
-                # Print status every 10 batches
-                if i % 10 == 0:
+                # Print status every batch
+                if i % 1 == 0:
                     time_elapsed = time.time() - start_time
                     avg_time_per_batch = time_elapsed / i
                     eta_seconds = avg_time_per_batch * (len(train_loader) - i)
@@ -83,60 +84,77 @@ def train():
             val_loss = 0.0
             with torch.no_grad():
                 for inputs, labels in val_loader:
+                    labels = labels.float()
                     outputs = model(inputs)
                     loss = criterion(outputs, labels)
                     val_loss += loss.item()
             val_loss /= len(val_loader)
             train_loss = running_loss / len(train_loader)
-
-            # Append losses for plotting
-            train_losses.append(running_loss / len(train_loader))
-            val_losses.append(val_loss)
             val_accuracy = compute_accuracy(model, val_loader)
-
-            # Early Stopping based on validation loss
-            early_stopping(val_loss, train_loss, val_accuracy, model)
-            if early_stopping.early_stop:
-                print("\nEarly stopping")
-                break
 
             # Validation Phase for Accuracy
             print(
                 f"Epoch [{epoch+1}/{config.num_epochs}], Validation Accuracy: {val_accuracy:.2f}%, Val Loss: {val_loss:.2e}"
             )
-            test_accuracy = compute_accuracy(model, test_loader)
-            print(f"Test Accuracy: {test_accuracy:.2f}%")
 
-            # Log metrics to wandb
-            wandb.log(
-                {
-                    "Train Loss": running_loss / len(train_loader),
-                    "Validation Loss": val_loss,
-                    "Validation Accuracy": val_accuracy,
-                    "Test Accuracy": test_accuracy,
-                }
-            )
+            # Early Stopping based on validation loss
+            early_stopping(val_loss, train_loss, val_accuracy, model)
+            if early_stopping.early_stop:
+                print("\nEarly stopping")
+                best_metrics = early_stopping.get_best_metrics()
+                break
+
+        # If training completes without early stopping, retrieve the best metrics
+        if not early_stopping.early_stop:
+            best_metrics = early_stopping.get_best_metrics()
+
+        # Log the best metrics to wandb
+        if best_metrics:
+            wandb.log(best_metrics)
 
 
 def evaluate_model(run_name):
     # load config file
     with open("params_eval.yaml", "r") as file:
         config = yaml.safe_load(file)["parameters"]
-        config = {key: value["values"][0] for key, value in config.items()}
+        config = {key: value["values"][0] if key != 'classes' else value["values"] for key, value in config.items()}
 
     # Load the best model
     filename = config["model"]
     path = f"models/{run_name}_{filename}.pt"
 
     # load .pt model
-    # model = torch.load(path)
     model, _, _ = initialize_CNN(config)
     model.load_state_dict(torch.load(path))
 
-    train, val, test = load_data(3)
+    test = load_test_data()
     test_loader = DataLoader(test, shuffle=False)
-    pred = process_images(model, test_loader)
-    report = classification_report(test.Target, pred)
+
+    true_labels =[]
+
+    for _, labels in test_loader:
+        true_labels.append(labels)
+    true_labels = torch.cat(true_labels, dim=0)
+
+    if len(true_labels.shape) > 1:  # Check if labels are one-hot encoded
+        true_labels = torch.max(true_labels, 1)[1]  # Convert to class indices
+
+    pred_probs = process_images(model, test_loader)
+    pred_probs = np.array(pred_probs)
+
+    # model architecture
+    visualize_model_architecture(model, input_shape=(1, 1, 224, 224))
+
+    # Generate report
+    pred_labels = np.argmax(pred_probs, axis=1)  # Convert probabilities to class labels
+    report = classification_report(true_labels.numpy(), pred_labels, target_names=config['classes'])
+    print(report)
+
+    # Plot ROC curves
+    plot_roc_curves(true_labels.numpy(), pred_probs, config['classes'])
+    plot_precision_recall_curves(true_labels.numpy(), pred_probs, config['classes'])
+    plot_calibration_curves_with_bootstrap(true_labels.numpy(), pred_probs, config['classes'])
+
     return report
 
 
@@ -144,20 +162,22 @@ def evaluate_model(run_name):
 # Main hyperparameter tuning loop
 ####################
 
-# Load config file
-with open("params.yaml") as f:
-    sweep_config = yaml.safe_load(f)
+# # Load config file
+# with open("params.yaml") as f:
+#     sweep_config = yaml.safe_load(f)
 
-# Initialize the sweep
-sweep_id = wandb.sweep(sweep_config, project="Kaggle ASCII Competition")
+# # Initialize the sweep
+# print("Initializing sweep")
+# sweep_id = wandb.sweep(sweep_config, project="MMD6020 NIH Chest X-Ray")
 
-# Start the sweep
-wandb.agent(sweep_id, train)
+# # Start the sweep
+# print("Starting sweep")
+# wandb.agent(sweep_id, train)
 
 ####################
 # Evaluate model
 ####################
 
-# load saved model from .pt file
-# run_name='trim-sweep-20'
-# evaluate_model(run_name)
+#load saved model from .pt file
+run_name='stellar-sweep-3'
+evaluate_model(run_name)
